e9906da45edbf07daf3e4f228fe549bb7b3166a1
Chris Del <drumgod101@gmail.com>
2018-10-22 17:05:23 -0400

    pull out editor and uploader

---
+++ src/components/Uploader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dropzone from 'react-dropzone';
+
+class Uploader extends React.Component {
+    render() {
+        return (
+            <div className="uploader">
+            <Dropzone onDrop={this.props.onDrop} >Drop some files here</Dropzone>
+            </div>
+
+        )
+    }
+}
+
+export default Uploader
